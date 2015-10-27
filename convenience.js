0b4a3f0263c92c2652a352510e63ed8fbdc623c5
Jonatan Zeidler <jonatan_zeidler@gmx.de>
2015-10-27 13:50:50 -0200

    Localization support + German Translation

--- convenience.js
+++ convenience.js
@@ -1,6 +1,8 @@
 /*global imports, print */
 const ExtensionUtils = imports.misc.extensionUtils;
 const Gio = imports.gi.Gio;
+const Gettext = imports.gettext;
+const Config = imports.misc.config;
 
 // copied from https://github.com/projecthamster/shell-extension/blob/f1f1d803395bc122db1b877985e1d2462c5215a9/convenience.js#L65
 function getSettings() {
@@ -18,4 +20,27 @@ function getSettings() {
                     + extension.metadata.uuid + '. Please check your installation.');
   let settings = new Gio.Settings({ settings_schema: schemaObj });
   return settings;
+}
+
+/**
+ * initTranslations:
+ * @domain: (optional): the gettext domain to use
+ *
+ * Initialize Gettext to load translations from extensionsdir/locale.
+ * If @domain is not provided, it will be taken from metadata['gettext-domain']
+ */
+function initTranslations(domain) {
+    let extension = ExtensionUtils.getCurrentExtension();
+
+    domain = domain || extension.metadata['gettext-domain'];
+
+    // check if this extension was built with "make zip-file", and thus
+    // has the locale files in a subfolder
+    // otherwise assume that extension has been installed in the
+    // same prefix as gnome-shell
+    let localeDir = extension.dir.get_child('locale');
+    if (localeDir.query_exists(null))
+        Gettext.bindtextdomain(domain, localeDir.get_path());
+    else
+        Gettext.bindtextdomain(domain, Config.LOCALEDIR);
 }
--- prefs.js
+++ prefs.js
@@ -4,16 +4,21 @@ const Gtk = imports.gi.Gtk;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Convenience = ExtensionUtils.getCurrentExtension().imports.convenience;
 
+const Gettext = imports.gettext.domain('switcher');
+const _ = Gettext.gettext;
+
 let entry, settings;
 
-function init() {}
+function init() {
+  Convenience.initTranslations("switcher");
+}
 
 function buildPrefsWidget() {
   let widget = new Gtk.VBox();
   settings = Convenience.getSettings();
   widget.margin = 10;
   let label = new Gtk.Label({margin_top: 20});
-  label.set_markup("<b>Hotkey to activate switcher</b>");
+  label.set_markup("<b>"+_("Hotkey to activate switcher")+"</b>");
   label.set_alignment(0, 0.5);
   widget.add(label);
   entry = new Gtk.Entry({margin_bottom: 10,
