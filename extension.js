88e30644fcf3e53748ce1b8f6e92b24be4234a7c
Nunes <maxnevesnunes@gmail.com>
2016-02-08 20:41:22 +0000

    Appearance tweaks
    These changes address the following points:
    - 1px gap between entry box and labels removed by styling the
      box layout;
    - highlight was being applied in the parent of labels, so a solid
      background color made the highlight invisible. Now styling is
      applied to box layout and each box instead of labels and icons;
    - Some colors were adjusted (such as highlight) to be closer to
      other GNOME Shell elements.

--- extension.js
+++ extension.js
@@ -72,29 +72,31 @@ function _hideUI() {
 
 function makeBox(app, index) {
   const fontSize = Convenience.getSettings().get_uint('font-size');
+
   const box = new St.BoxLayout({style_class: 'switcher-box'});
+  
   let shortcutBox = undefined;
   if (getActionKeyTable().length > 0) {
     const shortcut = new St.Label({
       style_class: 'switcher-shortcut',
       text: getKeyDesc(index + 1)
     });
-    shortcut.set_style('font-size: '+fontSize+'px');
     shortcutBox = new St.Bin({style_class: 'switcher-label'});
     shortcutBox.child = shortcut;
     box.insert_child_at_index(shortcutBox, 0);
   }
   const label = new St.Label({
     style_class: 'switcher-label',
+    y_align: Clutter.ActorAlign.CENTER,
     text: description(app)
   });
-  label.set_style('font-size: '+fontSize+'px');
   const iconBox = new St.Bin({style_class: 'switcher-icon'});
   const appRef = Shell.WindowTracker.get_default().get_window_app(app);
   iconBox.child = appRef.create_icon_texture(fontSize);
   box.insert_child_at_index(label, 0);
   label.set_x_expand(true);
   box.insert_child_at_index(iconBox, 0);
+
   return {whole: box, shortcutBox: shortcutBox};
 }
 
@@ -127,11 +129,14 @@ function _showUI() {
     }
   }, Convenience.getSettings().get_uint('activate-after-ms'));
 
-  container = new St.Bin({reactive: true});
   cursor = 0;
 
-  let boxLayout = new St.BoxLayout({style_class: 'switcher-box-layout'});
+  container = new St.Bin({reactive: true});
   container.set_alignment(St.Align.MIDDLE, St.Align.START);
+
+  const fontSize = Convenience.getSettings().get_uint('font-size');
+  let boxLayout = new St.BoxLayout({style_class: 'switcher-box-layout'});
+  boxLayout.set_style('font-size: ' + fontSize + 'px');
   boxLayout.set_vertical(true);
 
   // Get all windows in activation order
@@ -158,13 +163,12 @@ function _showUI() {
   let monitor = Main.layoutManager.primaryMonitor;
   container.set_width(monitor.width);
   container.set_height(monitor.height);
-  container.set_position(monitor.x, monitor.y);
+  container.set_position(monitor.x, 200);
 
   let width = boxes.map(box => box.whole.width).reduce((a, b) => Math.max(a, b), 0);
   let shortcutWidth = boxes
         .map(box => box.shortcutBox ? box.shortcutBox.width : 0)
         .reduce((a, b) => Math.max(a, b), 0);
-
   const maxWidth = Main.layoutManager.primaryMonitor.width * 0.01 *
           Convenience.getSettings().get_uint('max-width-percentage');
   if (width > maxWidth) width = maxWidth;
