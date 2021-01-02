604c58a5c526f67543a2c1f5022f63fef6328c54
Daniel Landau <daniel@landau.fi>
2021-01-02 23:32:12 +0200

    Move cursor to beginning when text changes

--- extension.js
+++ extension.js
@@ -353,6 +353,8 @@ function _showUI() {
     }
     // Filter text
     else {
+      // Cursor starts from 1 to allow quick switching, but should revert back to 0 when text changes
+      if (previousEntryContent !== entryContent) cursor = 0;
       rerunFiltersAndUpdate(o);
     }
 
